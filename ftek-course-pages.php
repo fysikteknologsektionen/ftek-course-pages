a01636e82dea532fc81cc7b4b2a5b38e9277ecc0
Johan Winther <johan.winther@outlook.com>
2018-08-20 10:32:58 +0200

    Added files

---
+++ ftek-course-pages.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Plugin Name: Ftek Course Pages
+Description: Creates custom post type "Course pages" which contain info about courses at F/TM on Chalmers
+Author: Pontus Granström
+Text Domain: ftekcp
+Domain Path: /languages
+GitHub Plugin URI: Fysikteknologsektionen/ftek-course-pages
+*/
+
+require_once('src/ftek-course-pages-init.php');
+require_once('src/ftek-course-pages-functions.php');
+require_once('src/ftek-course-pages-fields.php');
+require_once('src/ftek-course-pages-widget.php');
+require_once('src/ftek-course-pages-settings.php');
+require_once('src/ftek-course-pages-table.php');
